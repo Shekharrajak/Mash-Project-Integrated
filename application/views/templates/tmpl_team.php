3754155deb0632a1de82226a754586c724a70960
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-07-20 14:17:41 +0530

    team integrated

---
+++ application/views/templates/tmpl_team.php
@@ -0,0 +1,16 @@
+
+
+<?= $team_content ?>
+       <!--  <li>
+            <div class="ch-item">             
+                <div class="ch-info">
+                    <div class="ch-info-front ch-mayank"></div>
+                    <div class="ch-info-back">
+                        <h4>Developer,Epicure,Gadget-freak,Foodie,Sarcastic being
+                        </h4>
+                    </div>    
+                </div>
+            </div>
+                        <h5>Mayank Bhola</h5>
+                        <p>Advisor<br>Development<br><a href="http://about.me/bholamayank" target="_blank" class="btn btn-primary">Know More</a></p>
+        </li><br> -->
